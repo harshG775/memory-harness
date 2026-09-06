b71e0a524323167be3f1f8703c68d8ad378394f6
Harsh_G <hgaur491@gmail.com>
2026-09-06 22:39:12 +0530

    //

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,3 +1,4 @@
+import { Button } from "#/components/ui/button"
 import { createFileRoute } from "@tanstack/react-router"
 
 export const Route = createFileRoute("/")({
@@ -5,5 +6,10 @@ export const Route = createFileRoute("/")({
 })
 
 function RouteComponent() {
-    return <div>Hello "/"!</div>
+    return (
+        <div>
+            Hello "/"!
+            <Button>click me</Button>
+        </div>
+    )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -6,12 +6,14 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite"
 import viteReact, { reactCompilerPreset } from "@vitejs/plugin-react"
 import babel from "@rolldown/plugin-babel"
 import { nitro } from "nitro/vite"
+import tailwindcss from "@tailwindcss/vite"
 
 const config = defineConfig({
     resolve: { tsconfigPaths: true },
     plugins: [
         devtools(),
         nitro({ rollupConfig: { external: [/^@sentry\//] } }),
+        tailwindcss(),
         tanstackStart(),
         viteReact(),
         babel({ presets: [reactCompilerPreset()] }),
