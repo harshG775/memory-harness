0a9cdfcdc217bac3c8e34a032edafa3fd5fba77e
Harsh_G <hgaur491@gmail.com>
2026-09-15 22:47:55 +0530

    setting up cloudflare deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,22 +3,25 @@ import tailwindcss from "@tailwindcss/vite";
 import { devtools } from "@tanstack/devtools-vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact, { reactCompilerPreset } from "@vitejs/plugin-react";
-import { nitro } from "nitro/vite";
+// import { nitro } from "nitro/vite";
+import { cloudflare } from "@cloudflare/vite-plugin";
+
 import { defineConfig } from "vite";
 
 const config = defineConfig({
 	resolve: { tsconfigPaths: true },
 	plugins: [
 		devtools(),
-		nitro({ rollupConfig: { external: [/^@sentry\//] } }),
+		// nitro({ rollupConfig: { external: [/^@sentry\//] } }),
+		cloudflare({ viteEnvironment: { name: "ssr" } }),
 		tailwindcss(),
 		tanstackStart(),
 		viteReact(),
 		babel({ presets: [reactCompilerPreset()] }),
 	],
-	server: {
-		allowedHosts: ["memory-harness.harshgaur.in"],
-	},
+	// server: {
+	// 	allowedHosts: ["memory-harness.harshgaur.in"],
+	// },
 });
 
 export default config;
